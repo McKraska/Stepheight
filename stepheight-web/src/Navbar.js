f3c1ecdb387e92e959ea74624ad24ec7e70be145
Oskar Kraska McKone <McKraska@users.noreply.github.com>
2023-01-16 16:01:13 +0100

    navbar tests

---
+++ stepheight-web/src/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Navbar.css';
+
+export default function Navbar() {
+    return (
+        <div id='Nav'>
+            <a>Test 2</a>  
+        
+        </div>
+        
+
+    )
+}
