196c4dbd79e1c34a96a6ddcb413340e775d2dcaa
Max-Mckone <max.mckone@gmail.com>
2024-08-21 20:44:44 +0200

    fix: text spacing aboutus

--- src/components/AboutUsContent.js
+++ src/components/AboutUsContent.js
@@ -9,13 +9,20 @@ export default function AboutUsContent() {
             <h1>About Us</h1>
 
             <h3>
-            Three indie devs, setting out to create innovative games, hand in hand with their community!
+                Three indie devs, setting out to create innovative games, hand in hand with their community!
             </h3>
 
             <img src={banner1} alt="banner" className="banner" />
 
             <p>
-            Forming for what was originally a university project, Stepheight is one of a handful of developers trying to make games in Thuringia. Driven by an eagerness to create games in a region where very few people have tried so far, we seek to show that anybody can make games anywhere, regardless of background or location. Our dedication stems from our desire to build a community, locally but also online. We hope to cultivate an active following that wants to interact with us and help make our games be the best it can be.
+                Forming for what was originally a university project, Stepheight is one of a handful of developers trying to make games in Thuringia.
+            </p>
+            <p>
+                Driven by an eagerness to create games in a region where very few people have tried so far, we seek to show that anybody can make games anywhere, regardless of background or location.
+            </p>
+            <p>
+                Our dedication stems from our desire to build a community, locally but also online.
+                We hope to cultivate an active following that wants to interact with us and help make our games be the best it can be.
             </p>
 
         </div>
