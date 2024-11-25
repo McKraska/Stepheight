f441fc30f346f4a4e93cba72c576215dc2f0dfdf
Max McKone <max.mckone@gmail.com>
2024-11-25 18:09:20 +0100

    fix: forced the page to scroll to the top of the page when reloaded

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-// src/App.js
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import { ThemeProvider } from './contexts/ThemeContext';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
@@ -9,11 +8,20 @@ import KickingBrass from './pages/KickingBrass';
 import AboutUs from './pages/AboutUs';
 import Impressum from './pages/Impressum';
 
+function ScrollToTop() {
+    const location = useLocation();
+    useEffect(() => {
+        window.scrollTo(0, 0);
+    }, [location]);
+    return null;
+}
+
 function App() {
     return (
         <ThemeProvider>
             <Router>
                 <Navbar />
+                <ScrollToTop />
                 <Routes>
                     <Route path="/" element={<Home />} />
                     <Route path="/kickingbrass" element={<KickingBrass />} />
